99953b53cd7e280e47a556e883059e9180d44799
Jaeseong Park <wotjd0860@naver.com>
2024-03-22 22:22:25 -0600

    Sart of MS learn C# course

--- CSharpPracticeOne/Program.cs
+++ CSharpPracticeOne/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // This is the comment in C#
+
+            Console.WriteLine("This is the first line");
+
+            Console.Write("This is ");
+            Console.Write("the ");
+            Console.Write("second line");
         }
     }
 }
